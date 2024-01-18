c2344418bea7cb989c56935381251bfb9d8bc53d
Jake0144 <148715991+Jake0144@users.noreply.github.com>
2024-01-18 14:01:43 -0700

    Server side & main page

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const fs = require('fs');
+const ejs = require('ejs');
+
+const app = express();
+const port = 3000;
+
+//set ejs engine
+app.set('view engine', 'ejs')
+
+//middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+    app.render('index')
+  });
+
+
+//start server
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
