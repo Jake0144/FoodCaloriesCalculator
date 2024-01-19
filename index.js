3e1a67360e7cdfd60412938b3f32eab4b05ffc51
Jake0144 <148715991+Jake0144@users.noreply.github.com>
2024-01-18 17:27:22 -0700

    Login starter

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
-const fs = require('fs');
+const User = require('./models/user');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const session = require('express-session');
 const ejs = require('ejs');
 const path = require('path');
 const Food = require('./models/food');
@@ -12,28 +15,41 @@ const port = 3000;
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
+// Use session to track user login status
+app.use(session({ secret: process.env.MY_SECRET, resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
 //middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+//login protection
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+      return next();
+  }
+  res.redirect('/login');
+}
+
 
 //Get requests
-app.get('/', (req, res) => {
+app.get('/', isLoggedIn,(req, res) => {
     res.render('index');
   });
-app.get('/create-new-food', (req, res) => {
+app.get('/create-new-food',isLoggedIn, (req, res) => {
     res.render('addFood');
   });
-  app.get('/view-all-foods', async (req, res) => {
+  app.get('/view-all-foods', isLoggedIn,async (req, res) => {
     try {
         const foods = await Food.find();
-        console.log(foods); // Log the fetched data
+        console.log(foods); 
         res.render('viewFoods', { foods });
     } catch (err) {
         console.error(err);
         res.send(err);
     }
 });
-app.get('/view-all-food', (req, res) => {
+app.get('/view-all-food', isLoggedIn,(req, res) => {
     res.redirect('/view-all-foods');
   });
 //Post requests
@@ -45,6 +61,41 @@ app.post('/api/add-food', (req,res)=>{
       res.redirect('/');
     })
   });
+
+
+// Routes for authentication
+app.get('/login', (req, res) => {
+  res.render('login'); 
+});
+
+app.post('/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/login'
+}));
+
+app.get('/register', (req, res) => {
+  res.render('register'); 
+});
+
+app.post('/register', (req, res) => {
+  User.register(new User({ username: req.body.username }), req.body.password, (err, user) => {
+      if (err) {
+          console.error(err);
+          return res.render('register'); 
+      }
+
+      passport.authenticate('local')(req, res, () => {
+          res.redirect('/'); 
+      });
+  });
+});
+
+// Logout route
+app.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect('/login');
+});
+
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, {
